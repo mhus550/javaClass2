d1a08731f398543ba8d5f282c421cae74f64acfe
mhus550 <120803297+mhus550@users.noreply.github.com>
2022-12-22 17:25:50 -0500

    added new file

--- sortArray2.java
+++ sortArray2.java
@@ -13,6 +13,8 @@ public class AbsVsRelativePath {
 	//Relative file path is a path from the place where code is running to your specific file
 	File myNotesRelativePath = new File("src/day50/notes.txt");
 	System.out.println("file exists: " + myNotesRelativePath.exists());
-	System.out.println(myNotesRelativePath.getAbsolutePath());	
+	System.out.println(myNotesRelativePath.getAbsolutePath());
+		
+		System.out.println("Mohammad did")
 	}
 }

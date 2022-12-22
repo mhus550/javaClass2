44bb6203aecc63f453364063db753285d0676d69
mohammad <mhussaini550@Mohammads-MacBook-Pro.local>
2022-12-22 17:35:06 -0500

    new code block

--- sortArray2.java
+++ sortArray2.java
@@ -5,14 +5,16 @@ import java.io.File;
 public class AbsVsRelativePath {
 	public static void main(String[] args) {
 		
-	//Absolute file path is a path from root directory to your specific file 
-	File myNotes = new File("/Users/thanyaratsaengsing/eclipse-workspace/sdet-java-b7/src/day50/notes.txt");
+	//Absolute file path is a path from root directory to your specific fi
+jFile myNotes = new File("/Users/thanyaratsaengsing/eclipse-workspace/sdet-java-b7/src/day50/notes.txt");
 	System.out.println("file exists: " + myNotes.exists()); //file exists: true
 	System.out.println(myNotes.getAbsolutePath()); ///Users/thanyaratsaengsing/eclipse-workspace/sdet-java-b7/src/day50/notes.txt
 	
 	//Relative file path is a path from the place where code is running to your specific file
 	File myNotesRelativePath = new File("src/day50/notes.txt");
 	System.out.println("file exists: " + myNotesRelativePath.exists());
-	System.out.println(myNotesRelativePath.getAbsolutePath());	
+	System.out.println(myNotesRelativePath.getAbsolutePath());fgdfgdfg
+fdgdfadfgfdagadf
+        System.out.println("person 2 added new line")	
 	}
 }
